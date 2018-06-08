f60d08eb41abc698cf85bb582e65dfca260f755e
TKM <tkm@santhu-macbook-pro.local>
2018-06-08 12:23:24 +0530

    Initial Commit

---
+++ GithubExamle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubExamle
+//
+//  Created by TKM on 08/06/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubExamle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubExamle
+//
+//  Created by TKM on 08/06/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
